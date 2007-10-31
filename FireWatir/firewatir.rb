e068b34708609469098a2d1047479e13159ca06c
angrez <angrez@2c8948d5-9e19-0410-bb25-d1963fc456a0>
2007-10-31 06:44:09 +0000

    adding version number for Firewatir
    git-svn-id: http://firewatir.googlecode.com/svn/trunk@124 2c8948d5-9e19-0410-bb25-d1963fc456a0

--- FireWatir/firewatir.rb
+++ FireWatir/firewatir.rb
@@ -150,6 +150,8 @@ module FireWatir
         # XPath Result type. Return only first node that matches the xpath expression.
         # More details: "http://developer.mozilla.org/en/docs/DOM:document.evaluate"
         FIRST_ORDERED_NODE_TYPE = 9
+        
+        VERSION = '1.1.1'
 
         # variable to check if firefox browser has been started or not. Currently this is
         # used only while starting firefox on windows. For other platforms you need to start
