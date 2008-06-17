ba63d6dce891ec95d83a8f8d03f13505d2b036a7
bpettichord <bpettichord@2c8948d5-9e19-0410-bb25-d1963fc456a0>
2008-06-17 22:10:49 +0000

    Applying support-button.patch.txt
    git-svn-id: http://firewatir.googlecode.com/svn/trunk@139 2c8948d5-9e19-0410-bb25-d1963fc456a0

--- FireWatir/htmlelements.rb
+++ FireWatir/htmlelements.rb
@@ -1222,6 +1222,10 @@ end
 #
 class Button < InputElement
     INPUT_TYPES = ["button", "submit", "image", "reset"] 
+    def locate
+			super
+			@o = @element.locate_tagged_element("button", @how, @what, self.class::INPUT_TYPES) unless @o
+		end
 end
 
 #
